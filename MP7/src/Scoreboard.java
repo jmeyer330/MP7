e3f7819c7933845e20d5d82d2d5539d943c09776
jmeyer330 <32105903+jmeyer330@users.noreply.github.com>
2017-12-09 18:25:13 -0600

    Add files via upload

---
+++ MP7/src/Scoreboard.java
@@ -0,0 +1,12 @@
+/**
+ * Scoreboard of 13 rounds for the yahtzee game
+ * @author jarod meyer
+ *
+ */
+public class Scoreboard {
+	/**
+	 * Make an array of 13 (number of rounds AND different scoretypes)
+	 */
+	private int[] roundScoring = new int[13];
+	
+}
